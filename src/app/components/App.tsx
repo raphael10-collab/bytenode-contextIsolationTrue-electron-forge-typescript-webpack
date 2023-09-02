2ba13432f2fcb7c3587aaf783eb033cafa4c1c1c
raphy <raphael.stonehorse@gmail.com>
2023-09-02 15:06:39 +0200

    commit

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,6 +4,21 @@ const Empty = React.lazy(() => import('./empty'))
 
 function App() {
 
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  replaceText('renderer', '👍')
+
+  if (__filename) {
+    replaceText('node-renderer', '👍')
+  }
+})
+
   return (
     <div className='container'>
       <React.Suspense fallback={<div>Loading...</div>}>
@@ -16,3 +31,8 @@ function App() {
 
 
 export default App;
+
+
+// Export to keep from being tree-shaken by Webpack
+export const SOURCE_TEST =
+  '*** If this text appears in your packaged app, it means Bytenode is NOT configured correctly! #SOURCE-TEST ***'
--- src/main/index.ts
+++ src/main/index.ts
@@ -60,3 +60,8 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+// Export to keep from being tree-shaken by Webpack
+export const SOURCE_TEST =
+  '*** If this text appears in your packaged app, it means Bytenode is NOT configured correctly! #SOURCE-TEST ***'
